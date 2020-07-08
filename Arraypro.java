3c4efa16650c23104813f8be67fcabbc4ad3a7e8
amiruddin1 <67670878+amiruddin1@users.noreply.github.com>
2020-07-08 08:07:56 +0530

    Create Arraypro.java

---
+++ Arraypro.java
@@ -0,0 +1,17 @@
+Public class Arraypro {
+Public static void main(string[] args){
+Int arr[][]= {{1,2,3},{4,5,6},{7,8,9}};
+For(int I=0; I<3; I++){
+For (int j=0; j<3; j++){
+arr[i][j]= arr[i][j]*arr[i][j]; //for multiplication
+}
+System.out.println();
+}
+For(int I=0; I<3; I++){
+For (int j=0; j<3; j++){
+System.out.println(arr[i][j]);
+}
+System.out.println();
+}
+}
+}
